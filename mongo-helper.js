d18aea9171d052f356f4c35c7fb88981dc099d59
Diego Sandrim <diego.sandrim@agilecontent.com>
2017-02-06 14:31:16 -0200

    Simplify

--- mongo-helper.js
+++ mongo-helper.js
@@ -2,57 +2,57 @@
 
 const MongoClient = require('mongodb').MongoClient;
 
+const mongoEndpoint = process.env['MONGO_DB'] || 'localhost';
+const userAgentCollection = 'useragent';
 
-class MongoHelper{
-    static connect(){
-        return new Promise((resolve, reject) => {
-            let mongoEndpoint = process.env['MONGO_DB'] || 'my-mongo';
+class MongoHelper {
+  static connect() {
+    return new Promise((resolve, reject) => {
+      return MongoClient.connect(`mongodb://${mongoEndpoint}:27017/nodejs`, function (err, db) {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(db);
+      });
+    });
+  }
 
-            return MongoClient.connect(`mongodb://${mongoEndpoint}:27017/nodejs`, function(err, db) {
-                if(err){
-                    return reject(err);
-                }
-                return resolve(db);
-            });    
-        });
-    }
-    
-    static migrate(db){
-        return new Promise((resolve, reject) => {
-            db.createCollection('useragent', function(err, collection) {
-                if(err){
-                    return reject(err);
-                }
-                return resolve(collection);
-            });
-        });
-    }
+  static migrate(db) {
+    return new Promise((resolve, reject) => {
+      db.createCollection(userAgentCollection, function (err, collection) {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(collection);
+      });
+    });
+  }
 
-    static insertUserAgent(db, userAgent){
-        return new Promise((resolve, reject) => {
-            let collection = db.collection('useragent');
-            return collection.insert({name: userAgent}, {w:1}, function(err, result) {
-                if(err){
-                    return reject(err);
-                }
+  static insertUserAgent(db, userAgent) {
+    return new Promise((resolve, reject) => {
+      let collection = db.collection(userAgentCollection);
+      return collection.insert({ name: userAgent }, { w: 1 }, function (err, result) {
+        if (err) {
+          return reject(err);
+        }
 
-                return resolve(result);
-            });
-        });
-    }
+        return resolve(result);
+      });
+    });
+  }
 
-    static getUserAgent(db){
-        return new Promise((resolve, reject) => {
-            let collection = db.collection('useragent');
-            return collection.find({}).toArray(function(err, result) {
-                if(err){
-                    return reject(err);
-                }
+  static getUserAgent(db) {
+    return new Promise((resolve, reject) => {
+      let collection = db.collection(userAgentCollection);
+      return collection.find({}).toArray(function (err, result) {
+        if (err) {
+          return reject(err);
+        }
 
-                return resolve(result);
-            });
-        });
-    }
+        return resolve(result);
+      });
+    });
+  }
 }
 
 module.exports = MongoHelper;
